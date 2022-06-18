5571931207f3b41737f476983eec61326390e728
Doljko <bayarsuren0310@gmail.com>
2022-06-18 18:15:53 +0800

    sasah

--- components/History/index.js
+++ components/History/index.js
@@ -8,9 +8,7 @@ export default function History(){
                     <img className="mb-4 mx-20 xs:w-60 md:w-96 " src="/img/minanoBack.png"/>
                 </div>
                 <div className="mx-20 flex-wrap">
-                   <p className=" xs:80 md:w-96">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-                     of Letraset sheets containing Lorem Ipsurem ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-                     of Letraset sheets containing Lorem Ipsum passincluding versions of Lorem Ipsum.</p>
+                   <p className=" xs:80 md:w-96">tezt</p>
                      <img className="my-4" src="" alt="Гарын үсэг"/>
                      <h1 className="border-l border-slate px-2 text-SecularOne text-xl font-bold">Mina Dermatologist</h1>
                      <h1>Ерөнхий эмч, Захирал</h1>
